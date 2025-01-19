3459fa606c65be25147e7bc2c4c7a96633771bee
shorifulsobuj <sobujaiom126@gmail.com>
2025-01-19 22:44:00 +0600

    Css Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Pokemon from "./pages/Pokemon"; // import Pokemon.jsx
+import { Pokemon } from "./Pokemon";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Pokemon />} /> {/* Set the Pokemon page as default */}
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+export const App = () => {
+  return <Pokemon />;
+};
